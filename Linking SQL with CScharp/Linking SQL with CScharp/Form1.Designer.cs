522ebdcfa9aee769a20c2c68543bfb8b8ed0b329
Lyceum <48872358+cab-aranw@users.noreply.github.com>
2020-08-16 00:35:15 -0400

    Connected
    1.0

---
+++ Linking SQL with CScharp/Linking SQL with CScharp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Linking_SQL_with_CScharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectButton = new System.Windows.Forms.Button();
+            this.InsertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SelectButton
+            // 
+            this.SelectButton.Location = new System.Drawing.Point(72, 288);
+            this.SelectButton.Name = "SelectButton";
+            this.SelectButton.Size = new System.Drawing.Size(150, 69);
+            this.SelectButton.TabIndex = 0;
+            this.SelectButton.Text = "Select";
+            this.SelectButton.UseVisualStyleBackColor = true;
+            this.SelectButton.Click += new System.EventHandler(this.SelectButton_Click);
+            // 
+            // InsertButton
+            // 
+            this.InsertButton.Location = new System.Drawing.Point(406, 288);
+            this.InsertButton.Name = "InsertButton";
+            this.InsertButton.Size = new System.Drawing.Size(182, 69);
+            this.InsertButton.TabIndex = 1;
+            this.InsertButton.Text = "Insert";
+            this.InsertButton.UseVisualStyleBackColor = true;
+            this.InsertButton.Click += new System.EventHandler(this.InsertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.InsertButton);
+            this.Controls.Add(this.SelectButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectButton;
+        private System.Windows.Forms.Button InsertButton;
+    }
+}
+
---
+++ Linking SQL with CScharp/Linking SQL with CScharp/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Linking_SQL_with_CScharp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connectionVar = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\w_ara\Downloads\ProductDB.mdf;Integrated Security=True;Connect Timeout=30");
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectButton_Click(object sender, EventArgs e)
+        {
+            connectionVar.Open();
+
+            SqlCommand commandVar = connectionVar.CreateCommand();
+            commandVar.CommandType = CommandType.Text;
+            commandVar.CommandText = "SELECT * FROM Product";
+            
+            commandVar.ExecuteNonQuery(); // Executes
+
+            DataTable dataTableVar = new DataTable();
+            SqlDataAdapter dataAdapterVar = new SqlDataAdapter(commandVar);
+            dataAdapterVar.Fill(dataTableVar);
+
+            foreach (DataRow dataRowVar in dataTableVar.Rows)
+            {
+                //labelFeedback.Text = dataRowVar["email"].ToString();
+                MessageBox.Show(dataRowVar["Description"].ToString() + "  " +dataRowVar["Units_On_Hand"].ToString());
+            }
+
+
+            connectionVar.Close();
+
+            MessageBox.Show("Success");
+        }
+
+        private void InsertButton_Click(object sender, EventArgs e)
+        {
+            connectionVar.Open();
+
+            SqlCommand commandVar = connectionVar.CreateCommand();
+            commandVar.CommandType = CommandType.Text;
+            commandVar.CommandText = "INSERT INTO Product values('10-41','Sausage','1','9999')";
+
+            commandVar.ExecuteNonQuery(); // Executes
+
+            connectionVar.Close();
+
+            MessageBox.Show("Insert Successfully");
+        }
+    }
+}
